2940f2eef858ba6a59716ef7d33f08e13860a4c2
Kieran Llarena <baymax709@gmail.com>
2023-12-20 16:00:48 -0500

    feat: added heart rate logging

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,43 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { readable } from 'svelte/store';
+
+const DAY3_URL = 'https://advent.sveltesociety.dev/data/2023/day-four.json';
+
+type HeartRate = {
+	heartRate: string;
+};
+
+type HeartRateError = {
+	statusCode: number;
+	heartRate: string;
+};
+
+const getData = async (): Promise<HeartRate | HeartRateError> => {
+	try {
+		const response = await fetch(DAY3_URL);
+		const heartRate = await response.json();
+		return heartRate;
+	} catch (err) {
+		return {
+			statusCode: 500,
+			heartRate: 'Internal Server Error'
+		};
+	}
+};
+
+export const heartRateStream = readable(
+	{
+		stream: getData
+	},
+	(set) => {
+		const interval = setInterval(() => {
+			set({
+				stream: getData
+			});
+		}, 1000);
+
+		return () => {
+			console.log('unsubscribing');
+			clearInterval(interval);
+		};
+	}
+);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { heartRateStream } from '$lib/index'
+	import { onDestroy } from 'svelte';
+
+  type HeartRate = {
+	  heartRate: string;
+  };
+
+  type HeartRateLog = HeartRate[]
+
+  let heartRateLog: HeartRateLog = []
+
+  type HeartRateStream = () => Promise<HeartRate>
+
+  const handleData = async (dataStream: HeartRateStream) => {
+    const response = await dataStream()
+    heartRateLog = [...heartRateLog, response]
+  }
+
+  $: logLength = heartRateLog.length
+
+  const unsubscribe = heartRateStream.subscribe((val) => {
+    handleData(val.stream)
+  })
+
+  onDestroy(unsubscribe)
+</script>
+
+<main>
+  <h1>Day 4 Advent of Svelte: Santa Heart Rate Logger</h1>
+  <p>Live logging of Santa's heart rate</p>
+  <table>
+    <tr>
+      <th>Time</th>
+      <th>Heart Rate</th>
+    </tr>
+    {#each Array(logLength) as _, i}
+      <tr>
+        <td>{new Date()}</td>
+        <td>{heartRateLog[i].heartRate}</td>
+      </tr>
+    {/each}
+  </table>
+</main>
+
+<style>
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+  
+  td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+  
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
+</style>
